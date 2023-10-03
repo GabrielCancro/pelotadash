01b33871c08f0eb511e21cd3eca32dd9344f2554
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2023-10-03 18:01:29 -0300

    update

--- prefabs/Camera2D.gd
+++ prefabs/Camera2D.gd
@@ -1,28 +1,14 @@
 extends Camera2D
 
+onready var posy = GC.GAME_SIZE.y / 2
+var speed_y_mult = 5
+var margin = 200
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-onready var CAMERA_STEP_SIZE = GC.GAME_SIZE.y / 2
-var CURRENT_STEP = 1
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	GC.CAMERA = self
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#$Camera2D.position.y = GC.PLAYER.position.y - (GC.PLAYER.position.y - GC.PLAYER.last_min_y) / 2 -20
-	
-#	position.y = GC.PLAYER.position.y
-	
-#	var media_player_position_y = GC.PLAYER.position.y
-#	var camera_step_y = floor( media_player_position_y/CAMERA_STEP_SIZE ) + 0
-#	position.y = camera_step_y*CAMERA_STEP_SIZE
-#	print("media:",media_player_position_y,"   step:",camera_step_y,"   pos:",position.y,"   GameSize:",GC.GAME_SIZE.y)
-
-	if(GC.PLAYER.position.y<CURRENT_STEP*CAMERA_STEP_SIZE-CAMERA_STEP_SIZE+50): CURRENT_STEP -= 1
-	if(GC.PLAYER.position.y>CURRENT_STEP*CAMERA_STEP_SIZE+CAMERA_STEP_SIZE-100): CURRENT_STEP += 1
-	position.y = CURRENT_STEP*CAMERA_STEP_SIZE - CAMERA_STEP_SIZE / 4
+	var dist = (GC.PLAYER.position.y - posy)
+	if abs(dist)>margin:
+		posy = posy + dist * delta * speed_y_mult
+		position.y = posy
