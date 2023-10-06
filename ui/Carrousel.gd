2f08b374fe0512984a1190df6041604a47e203df
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2023-10-06 18:44:18 -0300

    update

--- ui/Carrousel.gd
+++ ui/Carrousel.gd
@@ -1,43 +1,56 @@
 extends Control
 
+var carrousel_item_scene = preload("res://ui/ShopCarrouselItem.tscn")
 var carrousel_index = 1
-onready var orig_pos_left = $Left_ShopCarrouselItem.rect_position
-onready var orig_pos_middle = $Middle_ShopCarrouselItem.rect_position
-onready var orig_pos_right = $Right_ShopCarrouselItem.rect_position
+var current_carrousel_item
 
 func _ready():
 	$bt_left.connect("button_down",self,"carrousel_back")
 	$bt_right.connect("button_down",self,"carrousel_next")
+	fill_carrousel_items()
+	yield(get_tree().create_timer(.1),"timeout")
 	update_carrousel_data()
 
+func fill_carrousel_items():
+	var i = 0
+	for old in $ItemsList.get_children(): old.queue_free()
+	for pack in DG.BIOMEPACK_DATA:
+		var cr = carrousel_item_scene.instance()
+		cr.set_data( DG.BIOMEPACK_DATA[pack] )
+		cr.rect_position.x += (cr.rect_size.x + 30) * i
+		$ItemsList.add_child(cr)
+		i+=1
+
 func update_carrousel_data(type="biomes"):
-	$bt_left.modulate.a = .1
-	$Left_ShopCarrouselItem.visible = (carrousel_index>0)
-	if($Left_ShopCarrouselItem.visible): 
-		$bt_left.modulate.a = 1
-		var keyL = DG.BIOMEPACK_DATA.keys()[carrousel_index-1]
-		$Left_ShopCarrouselItem.set_data( DG.BIOMEPACK_DATA[keyL] )
 	
-	var keyM = DG.BIOMEPACK_DATA.keys()[carrousel_index]
-	$Middle_ShopCarrouselItem.set_data( DG.BIOMEPACK_DATA[keyM] )
-	
-	$bt_right.modulate.a = .1
-	$Right_ShopCarrouselItem.visible = (carrousel_index<DG.BIOMEPACK_DATA.size()-1)
-	if($Right_ShopCarrouselItem.visible): 
-		var keyR = DG.BIOMEPACK_DATA.keys()[carrousel_index+1]
-		$Right_ShopCarrouselItem.set_data( DG.BIOMEPACK_DATA[keyR] )
-		$bt_right.modulate.a = 1
+	print(carrousel_index)
+	if(current_carrousel_item): current_carrousel_item.set_selected(false)
+	current_carrousel_item = $ItemsList.get_children()[carrousel_index]
+	current_carrousel_item.set_selected(true)
+	if(carrousel_index>0):$bt_left.modulate.a = 1
+	else: $bt_left.modulate.a = .1
+	if(carrousel_index<DG.BIOMEPACK_DATA.size()-1): $bt_right.modulate.a = 1
+	else: $bt_right.modulate.a = .1
 	
 	for point in $HBox.get_children():
 		point.visible = (point.get_index()<DG.BIOMEPACK_DATA.size())
 		if point.visible:
 			if point.get_index() == carrousel_index: point.modulate = Color(1,1,0,1) 
 			else: point.modulate = Color(.6,.6,.6,1)
+	animate_item_list( current_carrousel_item )
+
+func animate_item_list(cr):
+	var desX = GC.GAME_SIZE.x/2 - current_carrousel_item.rect_position.x - current_carrousel_item.rect_size.x/2
+	var destine = Vector2(desX,$ItemsList.rect_position.y)
+	$Tween.interpolate_property($ItemsList,"rect_position",$ItemsList.rect_position,destine,.3,Tween.TRANS_QUAD,Tween.EASE_OUT)
+	$Tween.start()
 
 func carrousel_next():
-	if (carrousel_index<DG.BIOMEPACK_DATA.size()-1): carrousel_index += 1
-	update_carrousel_data()
+	if (carrousel_index<DG.BIOMEPACK_DATA.size()-1): 
+		carrousel_index += 1
+		update_carrousel_data()
 
 func carrousel_back():
-	if(carrousel_index>0): carrousel_index -= 1
-	update_carrousel_data()
+	if(carrousel_index>0): 
+		carrousel_index -= 1
+		update_carrousel_data()
--- ui/ShopCarrouselItem.gd
+++ ui/ShopCarrouselItem.gd
@@ -1,10 +1,10 @@
 extends Control
 
 var data
-
+var selected
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_selected(false,true)
 
 func set_data(_data):
 	data = _data
@@ -16,3 +16,16 @@ func set_data(_data):
 	$lb_red.text = ""
 	if data.req_pack>0: $lb_red.text += "Need "+str(data.req_pack)+" biome packs"
 	if "J" in data.req_power: $lb_red.text += "\nNeed JUMP power up!"
+
+func set_selected(val,forced=false):
+	if !forced && val==selected: return
+	var time = .3
+	if forced: time = 0.01
+	selected = val
+	if selected == true:
+		$Tween.interpolate_property(self,"modulate",modulate,Color(1,1,1,1),time)
+		$Tween.interpolate_property(self,"rect_position",rect_position,rect_position+Vector2(0,-25),time)
+	else:
+		$Tween.interpolate_property(self,"modulate",modulate,Color(.3,.3,.3,1),time)
+		$Tween.interpolate_property(self,"rect_position",rect_position,rect_position+Vector2(0,+25),time)
+	$Tween.start()
